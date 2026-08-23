49e4bf3ef9c49701eba810394dbf92bfa69c53e7
MauCalil <mauricio.calil2@gmail.com>
2026-08-22 21:32:42 -0300

    fix: wrap app in I18nextProvider to ensure translations load on production build

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./styles.css";
-import "./i18n";
+import i18n from "./i18n";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
